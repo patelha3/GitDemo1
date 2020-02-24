b151614001e676c3e827ab54a954e3264d247709
harshal <patelha3@kean.edu>
2020-02-24 14:31:30 -0500

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void PostJira()
+	{
+		System.out.println("PostJira");
+	}
+	@Test
+	public void TwitterDelete()
+	{
+		System.out.println("TwitterDelete");
+	}
+}
