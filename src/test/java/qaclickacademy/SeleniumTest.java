1c60124dcfb634eb9862e20f984ed3db13b08dd7
harshal <patelha3@kean.edu>
2020-02-24 17:28:24 -0500

    message added in selenium test

--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -13,6 +13,11 @@ public class SeleniumTest {
 	public void ElementsUI()
 	{
 		System.out.println("ElementsUI");
+		System.out.println("ElementsUI1");
+		System.out.println("ElementsUI2");
+		System.out.println("ElementsUI3");
+		System.out.println("ElementsUI4");
+		System.out.println("ElementsUI5");
 	}
 	@Test
 	public void ElementsUI1()
